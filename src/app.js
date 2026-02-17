a032c959f5401a83bea116156d4687c3a20b4545
sanchitrohella <sanchitrohella@gmail.com>
2026-02-17 19:50:05 +0530

    Routing and Request Handlers

--- src/app.js
+++ src/app.js
@@ -2,13 +2,23 @@ const express = require("express");
 
 const app = express();
 
-app.use("/", (req, res) => {
-  res.send("Hello World From the Server...");
+// app.use("/user", (req, res) => {
+//   res.send("Hello About From the Server...");
+// });
+
+app.get("/user/:userId", (req, res) => {
+  console.log(req.params);
+  res.send("Get User Data From the Server...");
 });
 
-app.use("/about", (req, res) => {
-  res.send("Hello About From the Server...");
+app.post("/user", (req, res) => {
+  res.send("Post User Data to the Server...");
 });
+
+app.delete("/user", (req, res) => {
+  res.send("Delete User Data from the Server...");
+});
+
 app.listen(7777, () => {
   console.log("Server is running on port 7777...");
 });
